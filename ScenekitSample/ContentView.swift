8e9a43fa0e42ba71d723440a71f25905ec5e4ed2
shinter61 <shinter61@gmail.com>
2021-12-03 08:23:03 +0900

    Initial Commit

---
+++ ScenekitSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScenekitSample
+//
+//  Created by 松本真太朗 on 2021/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScenekitSample/ScenekitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenekitSampleApp.swift
+//  ScenekitSample
+//
+//  Created by 松本真太朗 on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct ScenekitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
